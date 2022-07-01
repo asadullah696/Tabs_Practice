e90251c9f7222f614a35d1187a36f787b97e69d3
asadullah696 <mr.asadullah19@gmail.com>
2022-07-02 01:14:59 +0500

    JavaScript file for Assignment

---
+++ script.js
@@ -0,0 +1,70 @@
+var contentA = document.getElementById("tab1");
+var contentB = document.getElementById("tab2");
+var contentC = document.getElementById("tab3");
+var contentD = document.getElementById("tab4");
+var contentE = document.getElementById("tab5");
+
+function homeAr(){
+    document.getElementById("tab-content1").style.display = "block"
+    document.getElementById("tab1").style.backgroundColor = "green"
+    document.getElementById("tab-content2").style.display = "none"
+    document.getElementById("tab2").style.backgroundColor = "black"
+    document.getElementById("tab-content3").style.display = "none"
+    document.getElementById("tab3").style.backgroundColor = "black"
+    document.getElementById("tab-content4").style.display = "none"
+    document.getElementById("tab4").style.backgroundColor = "black"
+    document.getElementById("tab-content5").style.display = "none"
+    document.getElementById("tab5").style.backgroundColor = "black"
+
+}
+function servicesAr(){
+    document.getElementById("tab-content1").style.display = "none"
+    document.getElementById("tab1").style.backgroundColor = "black"
+    document.getElementById("tab-content2").style.display = "block"
+    document.getElementById("tab2").style.backgroundColor = "green"
+    document.getElementById("tab-content3").style.display = "none"
+    document.getElementById("tab3").style.backgroundColor = "black"
+    document.getElementById("tab-content4").style.display = "none"
+    document.getElementById("tab4").style.backgroundColor = "black"
+    document.getElementById("tab-content5").style.display = "none"
+    document.getElementById("tab5").style.backgroundColor = "black"
+    
+}
+function aboutAr(){
+    document.getElementById("tab-content1").style.display = "none"
+    document.getElementById("tab1").style.backgroundColor = "black"
+    document.getElementById("tab-content2").style.display = "none"
+    document.getElementById("tab2").style.backgroundColor = "black"
+    document.getElementById("tab-content3").style.display = "green"
+    document.getElementById("tab3").style.backgroundColor = "green"
+    document.getElementById("tab-content4").style.display = "none"
+    document.getElementById("tab4").style.backgroundColor = "black"
+    document.getElementById("tab-content5").style.display = "none"
+    document.getElementById("tab5").style.backgroundColor = "black"
+}
+function portfolioAr(){
+    document.getElementById("tab-content1").style.display = "none"
+    document.getElementById("tab1").style.backgroundColor = "black"
+    document.getElementById("tab-content2").style.display = "none"
+    document.getElementById("tab2").style.backgroundColor = "black"
+    document.getElementById("tab-content3").style.display = "none"
+    document.getElementById("tab3").style.backgroundColor = "black"
+    document.getElementById("tab-content4").style.display = "block"
+    document.getElementById("tab4").style.backgroundColor = "green"
+    document.getElementById("tab-content5").style.display = "none"
+    document.getElementById("tab5").style.backgroundColor = "black"
+}
+function contactAr(){
+    document.getElementById("tab-content1").style.display = "none"
+    document.getElementById("tab1").style.backgroundColor = "block"
+    document.getElementById("tab-content2").style.display = "none"
+    document.getElementById("tab2").style.backgroundColor = "black"
+    document.getElementById("tab-content3").style.display = "none"
+    document.getElementById("tab3").style.backgroundColor = "black"
+    document.getElementById("tab-content4").style.display = "none"
+    document.getElementById("tab4").style.backgroundColor = "black"
+    document.getElementById("tab-content5").style.display = "block"
+    document.getElementById("tab5").style.backgroundColor = "green"
+}
+
+
